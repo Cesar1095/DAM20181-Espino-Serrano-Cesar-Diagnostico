20f709653081b75bdfa6807fca9f2697a408915f
cesar <ceraespinose@ittepic.edu>
2018-01-31 00:21:22 -0700

    primer commit

---
+++ mijs.js
@@ -0,0 +1,9 @@
+function inicia(){
+       if (document.form.password.value=='admin' && document.form.usuario.value=='admin'){ 
+        document.form.submit(); 
+           		} 
+    	
+    			else{ 
+         				alert("Datos invalidos"); 
+    			} 
+} 
